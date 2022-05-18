dfcc8a77e3c1a473e9d192ecff2b53407dca0975
agarrigu <agarrigu@protonmail.com>
2022-05-18 02:55:26 +0200

    make problem 001 one-liner

--- pe_001.py
+++ pe_001.py
@@ -1,16 +1,9 @@
 # Find the sum of all the multiples of 3 or 5 below 1000
 # These are more fun to figure out on your own!
 
-'''
-This could be done in a comprehensive list XD, TODO
-'''
 def main():
-    sum = 0
-    for i in range(1000):
-        if i % 3 == 0 or i % 5 == 0:
-            sum += i
-
-    print(sum)
+    answer = sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0])
+    print(answer)
 
 if __name__ == '__main__':
     main()
